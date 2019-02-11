349f88eb8f4fc1b578d886dd19d74895f58fe1ee
Anton Serdyuchenko <anton415@gmail.com>
2019-02-11 06:14:32 +0300

    Tweet UI, part 1.

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { formatTweet } from '../utils/helpers'
+import { formatTweet, formatDate } from '../utils/helpers'
+import { TiArrowBackOutline, TiHeartOutline, TiHeartFullOutline } from 'react-icons/ti'
 
 class Tweet extends Component {
+  toParent = (e, id) => {
+    e.preventDefault()
+    // todo: Redirect to parent Tweet.
+  }
   render() {
     const { tweet } = this.props
 
@@ -10,11 +15,31 @@ class Tweet extends Component {
       return <p>This Tweet doesn't exist</p>
     }
 
-    console.log(this.porps);
+    const {
+      name, avatar, timestamp, text, hasLiked, likes, replies, id, parent
+    } = tweet
 
     return (
       <div className='tweet'>
-
+        <img
+          src={avatar}
+          alt={`Avatar of ${name}`}
+          className='avatar'
+        />
+        <div className='tweet-info'>
+          <span>{name}</span>
+          <div>{formatDate(timestamp)}</div>
+          {parent && (
+            <button className='repying-to' onClick={(e) => this.toParent(e, parent.id)}>
+              Replying to @{parent.author}
+            </button>
+          )}
+          <p>{text}</p>
+        </div>
+        <div className='tweet-icons'>
+          <TiArrowBackOutline className='tweet-icon' />
+          <span>{replies !== 0 && replies}</span>
+        </div>
       </div>
     )
   }
