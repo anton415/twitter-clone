285efc3c61935add3e5bfa0671a40ffd57c4153e
Anton Serdyuchenko <anton415@gmail.com>
2019-02-14 05:35:55 +0300

    Like Tweet Component.

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -2,11 +2,20 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { formatTweet, formatDate } from '../utils/helpers'
 import { TiArrowBackOutline, TiHeartOutline, TiHeartFullOutline } from 'react-icons/ti'
+import { handleToggleTweet } from '../actions/tweets'
 
 class Tweet extends Component {
   handleLike = (e) => {
     e.preventDefault()
-    // TODO: Handle like tweet.
+
+    const { dispatch, tweet, authedUser } = this.props
+
+    dispatch(handleToggleTweet({
+      id: tweet.id,
+      hasLiked: tweet.hasLiked,
+      authedUser
+    }))
+
   }
 
   toParent = (e, id) => {
